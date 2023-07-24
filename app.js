5cbc2dc60be78af60bdcf49c7fb5f84286c3e58f
Lucas Manfredini <lucasmanf.vaz@gmail.com>
2023-07-24 20:45:01 -0300

    console log indicando modificações do paulo

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação se conectando ao banco de dados")
+// mais um console log de teste:
+console.log("o Paulo fez esse commit sem voce ver na aula")
